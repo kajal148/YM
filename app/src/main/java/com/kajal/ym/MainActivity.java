e306691c3efb05fafd253bdcb9fe3b330dbfd159
Kajal Varma <148kajal@gmail.com>
2020-08-29 10:45:05 +0530

    Updated Tips Pop Up and Text Counter

--- app/src/main/java/com/kajal/ym/MainActivity.java
+++ app/src/main/java/com/kajal/ym/MainActivity.java
@@ -1,76 +1,153 @@
 package com.kajal.ym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mCountTextView;
+    EditText mCountEditText;
+
     Spinner days,nights;
     RecyclerView gallery,brochure;
     List<Image> imageList;
-    ImageView mGallery, mBrochure;
+    ImageView mGalleryClick, mBrochureClick;
     
     Dialog mDialog;
     EditText mEditField;
     String add_field;
-    
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
     private List<com.kajal.ym.AddList> mServicesList= new ArrayList<>();
     RecyclerView mServiceRecyclerView;
     private List<com.kajal.ym.AddList> mCategoryList= new ArrayList<>();
     RecyclerView mCategoryRecyclerView;
+
+    private EditText mSearchField;
+    private ImageButton mSearchBtn;
+    private RecyclerView mRecyclerView;
+    private DatabaseReference databaseReference;
+    private FirebaseUser firebaseUser;
+    ArrayList<Places> arrayList = new ArrayList<>();
+    SearchAdapter searchAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGallery = findViewById(R.id.icon_gallery);
-        mBrochure = findViewById(R.id.icon_brochure);
+        mGalleryClick = findViewById(R.id.icon_gallery);
+        mBrochureClick = findViewById(R.id.icon_brochure);
         mDialog = new Dialog(this);
 
-        mGallery.setOnClickListener(new View.OnClickListener() {
+        brochure = findViewById(R.id.brochure);
+        gallery = findViewById(R.id.gallery);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        mSearchField = (EditText) findViewById(R.id.search_field);
+        mSearchBtn = (ImageButton) findViewById(R.id.search_btn);
+
+        mCountEditText =  findViewById(R.id.edit_tripSubject);
+        mCountTextView = findViewById(R.id.text_tripCounter);
+
+        mCountEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                int length = mCountEditText.length();
+                String convert = String.valueOf(length);
+                if(length==0)
+                {
+                    mCountTextView.setText(" 0/100");
+                }else{
+                    mCountTextView.setText(" "+convert+"/100");
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
 
 
-                Intent imgIntent = new Intent();
-                imgIntent.setType("image/*");
-                imgIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                imgIntent.setAction(Intent.ACTION_GET_CONTENT);
+            //PLACE RECYCLE VIEW LIST
 
-                startActivityForResult(Intent.createChooser(imgIntent,"Select Photo"), 1);
+        mRecyclerView = (RecyclerView) findViewById(R.id.result_list);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+
+        //for search text change
+        mSearchField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (!s.toString().isEmpty()){
+                    setAdapter(s.toString());
+                }
             }
         });
 
-        mBrochure.setOnClickListener(new View.OnClickListener() {
+        // for search button click
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String s = mSearchField.getText().toString();
+                if (!s.toString().isEmpty()){
+                    setAdapter(s.toString());
+                }
 
-
-                Intent imgIntent = new Intent();
-                imgIntent.setType("image/*");
-                imgIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                imgIntent.setAction(Intent.ACTION_GET_CONTENT);
-
-                startActivityForResult(Intent.createChooser(imgIntent,"Select Photo"), 1);
             }
         });
+
+                //SPINNER FOR DAYS AND NIGHT
 
         days = findViewById(R.id.spinner_days);
         nights = findViewById(R.id.spinner_months);
@@ -93,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
                 if (!parent.getItemAtPosition(position).equals("Days")){
                     String item = parent.getItemAtPosition(position).toString();
                     //optional later remove it
-                    Toast.makeText(parent.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(parent.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
                 }
             }
             @Override
@@ -119,7 +196,7 @@ public class MainActivity extends AppCompatActivity {
                 if (!parent.getItemAtPosition(position).equals("Nights")){
                     String item = parent.getItemAtPosition(position).toString();
                     //optional later remove it
-                    Toast.makeText(parent.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(parent.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
                 }
             }
             @Override
@@ -129,12 +206,42 @@ public class MainActivity extends AppCompatActivity {
 
 
                                                     // RECYCLE VIEW FOR GALLERY AND BROCHURE
-        
-        brochure = findViewById(R.id.brochure);
-        gallery = findViewById(R.id.gallery);
+
+        mGalleryClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent imgIntent = new Intent();
+                imgIntent.setType("image/*");
+                imgIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                imgIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(imgIntent,"Select Photo"), 1);
+            }
+        });
+
+        mBrochureClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent imgIntent = new Intent();
+                imgIntent.setType("image/*");
+                imgIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                imgIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(imgIntent,"Select Photo"), 1);
+            }
+        });
+
 
         //for saving all the images
         imageList = new ArrayList<>();
+        imageList.add(new Image(R.drawable.ic_launcher_background));
+        imageList.add(new Image(R.drawable.ic_launcher_background));
+        imageList.add(new Image(R.drawable.ic_launcher_background));
+        imageList.add(new Image(R.drawable.ic_launcher_background));
         imageList.add(new Image(R.drawable.ic_launcher_background));
         imageList.add(new Image(R.drawable.ic_launcher_background));
 
@@ -154,7 +261,7 @@ public class MainActivity extends AppCompatActivity {
         AdapterClass g_adapter = new AdapterClass(this,imageList);
         gallery.setAdapter(g_adapter);
 
-                        //RECYCLEVIEW FOR ADDITIONAL SERVICES
+                        //RECYCLE VIEW FOR ADDITIONAL SERVICES
         
 
         mServiceRecyclerView = findViewById(R.id.services_list);
@@ -170,7 +277,7 @@ public class MainActivity extends AppCompatActivity {
         ListAdapterClass services_adapter = new ListAdapterClass(this,mServicesList);
         mServiceRecyclerView.setAdapter(services_adapter);
 
-                 //RECYCLEVIEW FOR CATEGORY
+                    //RECYCLEVIEW FOR CATEGORY
 
         mCategoryRecyclerView = findViewById(R.id.category_list);
 
@@ -182,9 +289,48 @@ public class MainActivity extends AppCompatActivity {
         manager_category.setOrientation(LinearLayoutManager.HORIZONTAL);
         mCategoryRecyclerView.setLayoutManager(manager_category);
 
-        ListAdapterClass services_category = new ListAdapterClass(this,mCategoryList);
-        mCategoryRecyclerView.setAdapter(services_adapter);
+        ListAdapterClass category_adapter = new ListAdapterClass(this,mCategoryList);
+        mCategoryRecyclerView.setAdapter(category_adapter);
         
+    }
+
+    private void setAdapter(final String searchString) {
+        databaseReference.child("Locations").addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //for every new search
+                arrayList.clear();
+                mRecyclerView.removeAllViews();
+
+                int counter = 0;
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    String name = snapshot.child("name").getValue(String.class);
+                    String link = snapshot.child("link").getValue(String.class);
+                    String description = snapshot.child("description").getValue(String.class);
+                    if (name.startsWith(searchString)){
+                        arrayList.add(new Places(name, link, description));
+                        counter++;
+                    }
+
+                    //for the top 15 results
+                    if (counter  == 15){
+                        break;
+                    }
+
+                }
+
+                searchAdapter = new SearchAdapter(MainActivity.this, arrayList, mSearchField,mRecyclerView);
+                mRecyclerView.setAdapter(searchAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void popUpService(View v){
@@ -192,12 +338,14 @@ public class MainActivity extends AppCompatActivity {
         mDialog.setContentView(R.layout.popup_text);
         mButton = mDialog.findViewById(R.id.btn_submit);
         mEditField = mDialog.findViewById(R.id.edit_services);
-        mEditField.setText("");
+        //mEditField.setText("");
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 add_field= mEditField.getText().toString();
-                mServicesList.add(new AddList(add_field));
+                if(add_field.length()!=0){
+                    mServicesList.add(new AddList(add_field));
+                }
                 //Toast.makeText(getApplicationContext(), add_field, Toast.LENGTH_SHORT).show();
                 mDialog.dismiss();
             }
@@ -210,16 +358,51 @@ public class MainActivity extends AppCompatActivity {
         mDialog.setContentView(R.layout.popup_text);
         mButton = mDialog.findViewById(R.id.btn_submit);
         mEditField= mDialog.findViewById(R.id.edit_services);
-        mEditField.setText("");
+        //mEditField.setText("");
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                add_field= mEditField.getText().toString();
-                mCategoryList.add(new AddList(add_field));
+                add_field = mEditField.getText().toString();
+                if(add_field.length()!=0){
+                    mCategoryList.add(new AddList(add_field));
+                }
                 //Toast.makeText(getApplicationContext(), add_field, Toast.LENGTH_SHORT).show();
                 mDialog.dismiss();
             }
         });
         mDialog.show();
     }
+
+    public void tripSubjectTip(View v){
+        final ImageView mImageView;
+        final TextView mtextView;
+        mDialog.setContentView(R.layout.tips_popup);
+        mImageView = mDialog.findViewById(R.id.tipButton);
+        mtextView= mDialog.findViewById(R.id.tipText);
+        mtextView.setText("write the subject of the trip");
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDialog.dismiss();
+            }
+        });
+        mDialog.show();
+    }
+
+    public void tripDescriptionTip(View v){
+        final ImageView mImageView;
+        final TextView mtextView;
+        mDialog.setContentView(R.layout.tips_popup);
+        mImageView = mDialog.findViewById(R.id.tipButton);
+        mtextView= mDialog.findViewById(R.id.tipText);
+        mtextView.setText("say a few words about your trip");
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDialog.dismiss();
+            }
+        });
+        mDialog.show();
+    }
+
 }
